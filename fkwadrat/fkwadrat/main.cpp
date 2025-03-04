021e106e25f74eb7b8be6899b1063077c7e95abd
Damian <damian.gorgon00@gmail.com>
2025-03-04 15:21:33 +0100

    Drugi Commit

--- fkwadrat/fkwadrat/main.cpp
+++ fkwadrat/fkwadrat/main.cpp
@@ -12,24 +12,3 @@ int main() {
     scanf_s("%lf", &b);
 
     printf("Podaj wspolczynnik c: ");
-    scanf_s("%lf", &c);
-
-    // Obliczenie delty
-    delta = b * b - 4 * a * c;
-
-    // Sprawdzenie liczby pierwiastków
-    if (delta > 0) {
-        x1 = (-b + sqrt(delta)) / (2 * a);
-        x2 = (-b - sqrt(delta)) / (2 * a);
-        printf("Rownanie ma dwa pierwiastki: x1 = %.2f, x2 = %.2f\n", x1, x2);
-    }
-    else if (delta == 0) {
-        x1 = -b / (2 * a);
-        printf("Rownanie ma jeden pierwiastek: x = %.2f\n", x1);
-    }
-    else {
-        printf("Brak pierwiastkow rzeczywistych (delta < 0)\n");
-    }
-
-    return 0;
-}
